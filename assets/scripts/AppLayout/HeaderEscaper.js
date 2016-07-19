4ad7f6179d03a15519dc236d6967bd55df2cfa25
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-07-20 03:29:56 +0900

    theme

--- assets/scripts/AppLayout/HeaderEscaper.js
+++ assets/scripts/AppLayout/HeaderEscaper.js
@@ -13,14 +13,22 @@ export default class extends HeaderClassController {
 
 	toggleClass() {
 		let currentPos = this.getScrollPosition();
-		if (currentPos > this.scrollPos && this.isExceedsThreshold() ) {
-			this.$header.addClass( this.classString );
-			this.$header.attr('aria-hidden', 'true');
-		}
-		else {
+
+		if( ! this.isExceedsThreshold() ) {
 			this.$header.removeClass( this.classString );
 			this.$header.attr('aria-hidden', 'false');
 		}
+		else if( Math.abs( currentPos - this.scrollPos ) > 40 ) {
+			if ( currentPos > this.scrollPos ) {
+				this.$header.addClass( this.classString );
+				this.$header.attr('aria-hidden', 'true');
+			}
+			else {
+				this.$header.removeClass( this.classString );
+				this.$header.attr('aria-hidden', 'false');
+			}
+		}
+
 		this.scrollPos = currentPos;
 	}
 
