83857343b3543951da9c9a13f5eead7220c28234
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-09-22 02:02:00 +0900

    add dist

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,29 +3,8 @@ import './gulp/tasks/stylus';
 import './gulp/tasks/browserify';
 import './gulp/tasks/server';
 import './gulp/tasks/watch';
+import './gulp/tasks/dist';
 
 gulp.task('build', ['stylus', 'browserify']);
 gulp.task('default', ['setWatch', 'build', 'watch', 'browserSync']);
 
-
-gulp.task('copy', function() {
-	return gulp.src(
-		[
-			'./**/*.php',
-			'./**/*.css',
-			'./**/*.js',
-			'./LICENSE',
-			'./**/*.json',
-			'./assets/**/*.*',
-			"!./release/**",
-			"!./node_modules/**/*.*"
-		],
-		{ base: './' }
-	)
-		.pipe( debug() )
-		.pipe( gulp.dest( 'release' ) );
-} );
-
-gulp.task('release', function(cb){
-	return runSequence( 'build', 'copy', cb );
-});
