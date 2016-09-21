51100d02b208921c00c813d5be5492beb5365733
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-09-22 02:27:54 +0900

    fix

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,22 +2,6 @@
 
 set -e
 
-if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
-	echo "Not deploying pull requests."
-	exit
-fi
-
-if [[ "master" != "$TRAVIS_BRANCH" ]]; then
-	echo "Not on the 'master' branch."
-	exit
-fi
-
-if [[ "5.3" != "$TRAVIS_PHP_VERSION" ]]; then
-	echo "deploy only 5.3"
-	exit
-fi
-
-
 git clone -b dist --quiet "https://github.com/${TRAVIS_REPO_SLUG}.git" dist
 
 npm run dist
