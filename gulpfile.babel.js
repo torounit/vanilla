b2ff4ae563ae1287448229954b5899376db55439
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-09-28 11:30:31 +0900

    fix gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,7 +4,7 @@ import './gulp/tasks/browserify';
 import './gulp/tasks/server';
 import './gulp/tasks/watch';
 import './gulp/tasks/dist';
-import './gulp/tasks/zip';
+import './gulp/tasks/release';
 
 gulp.task('build', ['stylus', 'browserify']);
 gulp.task('default', ['setWatch', 'build', 'watch', 'browserSync']);
