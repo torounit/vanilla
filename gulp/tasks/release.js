8b6bacd0bb94d06f6a70ecb3265282d952d6ece5
Takashi Kitajima <inc@2inc.org>
2016-10-11 23:43:29 +0900

    Update release.js

--- gulp/tasks/release.js
+++ gulp/tasks/release.js
@@ -9,7 +9,7 @@ import gulp from 'gulp';
 import zip from 'gulp-zip';
 
 
-gulp.task('release', [ 'dist'], function(){
+gulp.task('release', ['dist'], function(){
 	return gulp.src('dist/**/*', {base: 'dist'})
 		.pipe(zip('vanilla.zip'))
 		.pipe(gulp.dest('./'));
