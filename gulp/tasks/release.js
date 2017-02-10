d25167e127c19eb77c67353611c7eb2743a5d6d8
Hiroshi Urabe <mail@torounit.com>
2017-02-11 00:58:43 +0900

    zip filename in version

--- gulp/tasks/release.js
+++ gulp/tasks/release.js
@@ -7,10 +7,12 @@
 // ==================================
 import gulp from 'gulp';
 import zip from 'gulp-zip';
-
+import minimist from 'minimist';
 
 gulp.task('release', ['dist'], function(){
+	let env = minimist(process.argv.slice(2));
+	let version = env.v || '';
 	return gulp.src('dist/**/*', {base: 'dist'})
-		.pipe(zip('vanilla.zip'))
+		.pipe(zip('vanilla-'+version+'.zip'))
 		.pipe(gulp.dest('./'));
 });
