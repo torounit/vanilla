d2c1b720a01bb9941b49328310fcc679d1f08860
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-03-04 01:18:45 +0900

    fix scroll

--- assets/scripts/AppLayout/HeaderEscaper.js
+++ assets/scripts/AppLayout/HeaderEscaper.js
@@ -17,15 +17,15 @@ export default class extends HeaderClassController {
 			this.$header.removeClass( this.classString );
 			this.$header.attr('aria-hidden', 'false');
 		}
-		else if( Math.abs( currentPos - this.scrollPos ) > 15 ) {
-			if ( currentPos > this.scrollPos ) {
-				this.$header.addClass( this.classString );
-				this.$header.attr('aria-hidden', 'true');
-			}
-			else {
-				this.$header.removeClass( this.classString );
-				this.$header.attr('aria-hidden', 'false');
-			}
+		else if(  currentPos - this.scrollPos > 5 ) {
+			//scroll to down
+			this.$header.addClass( this.classString );
+			this.$header.attr('aria-hidden', 'true');
+		}
+		else if (  currentPos - this.scrollPos < - 5 ) {
+			//scroll to up
+			this.$header.removeClass( this.classString );
+			this.$header.attr('aria-hidden', 'false');
 		}
 
 		this.scrollPos = currentPos;
