79ca5f3e033f9f0d45acd63bac0b9561cde0ff71
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-03-06 21:53:26 +0900

    fix widget

--- index.php
+++ index.php
@@ -12,10 +12,6 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<?php do_action( 'vanilla_site_main_prepend' ); ?>
 
-			<?php if ( is_active_sidebar( 'site-main-head-widget' ) ) : ?>
-				<?php dynamic_sidebar( 'site-main-head-widget' ); ?>
-			<?php endif; ?>
-
 			<?php
 			if ( have_posts() ) :?>
 				<?php if ( ! is_front_page() ) : ?>
@@ -54,7 +50,9 @@ get_header(); ?>
 			endif; ?>
 
 			<?php if ( is_active_sidebar( 'site-main-tail-widget' ) ) : ?>
-				<?php dynamic_sidebar( 'site-main-tail-widget' ); ?>
+				<div class="container">
+					<?php dynamic_sidebar( 'site-main-tail-widget' ); ?>
+				</div>
 			<?php endif; ?>
 
 			<?php do_action( 'vanilla_site_main_append' ); ?>
