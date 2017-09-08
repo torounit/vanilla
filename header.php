0fd6ba792cdc9a1f2badd2a91ce0345455ee33d3
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-09-08 11:16:05 +0900

    header.php fix

--- header.php
+++ header.php
@@ -23,11 +23,7 @@
 <body <?php body_class(); ?>>
 <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'vanilla' ); ?></a>
 
-<div id="page" class="site [ app-layout
-<?php
-if ( ! is_front_page() ) :
-?>
-app-layout--disable<?php endif; ?> ]" data-app-layout-header=".app-layout__header" data-app-layout-spacer=".app-layout__spacer" data-app-layout-scroll-area="window">
+<div id="page" class="site [ app-layout <?php if ( ! is_front_page() ) : ?>app-layout--static-header<?php endif; ?> ]" data-app-layout-header=".app-layout__header" data-app-layout-spacer=".app-layout__spacer" data-app-layout-scroll-area="window">
 	<header class="app-layout__header" aria-hidden="false" role="banner">
 		<div class="navbar container">
 			<div  class="navbar__branding">
