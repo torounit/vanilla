8a34ef2bdac8307893fffa1a844f2df00fe94703
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-10-09 03:58:42 +0900

    node 8

--- gulp/tasks/backstop.js
+++ gulp/tasks/backstop.js
@@ -6,19 +6,24 @@ let server = new connect();
 
 const run = (command) => {
 	const env = require(  '../../.env.json' );
-	return server.server({
-		port: env.server.port,
-		host: env.server.host,
-		base: './server',
-		router: './vendor/wp-cli/server-command/router.php'
-	}, () => {
-		backstopjs(command).then(() => {
-			server.closeServer();
-		}).catch(() => {
-			server.closeServer();
-			process.exit(1)
-		});;
-	});
+	try {
+		return server.server({
+			port: env.server.port,
+			host: env.server.host,
+			base: './server',
+			router: './vendor/wp-cli/server-command/router.php'
+		}, () => {
+			backstopjs(command).then(() => {
+				return server.closeServer();
+			}).catch(() => {
+				server.closeServer();
+				return process.exit(1)
+			});
+		});
+	}
+	catch (error) {
+		return process.exit(1)
+	}
 };
 
 gulp.task('test', ['build', 'cloneTheme'] , () => {
