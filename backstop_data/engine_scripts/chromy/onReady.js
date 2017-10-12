37ce93bd2bc9d07292bab1c8987da3d698ca9142
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-10-12 17:32:47 +0900

    test

--- backstop_data/engine_scripts/chromy/onReady.js
+++ backstop_data/engine_scripts/chromy/onReady.js
@@ -1,8 +1,8 @@
 module.exports = function (chromy, scenario, vp) {
   console.log('SCENARIO > ' + scenario.label);
   require('./clickAndHoverHelper')(chromy, scenario);
-  // chromy.evaluate(() => {
-	//   return document.querySelector('html').classList.add('mode-backstopjs');
-  // })
+  chromy.evaluate(() => {
+	  return document.querySelector('html').classList.add('mode-backstopjs');
+  })
   // add more ready handlers here...
 };
--- gulp/tasks/backstop.js
+++ gulp/tasks/backstop.js
@@ -6,24 +6,21 @@ let server = new connect();
 
 const run = (command) => {
 	const env = require(  '../../.env.json' );
-	try {
-		return server.server({
-			port: env.server.port,
-			host: env.server.host,
-			base: './server',
-			router: './vendor/wp-cli/server-command/router.php'
-		}, () => {
-			backstopjs(command).then(() => {
-				return server.closeServer();
-			}).catch(() => {
-				server.closeServer();
-				return process.exit(1)
-			});
+
+	return server.server({
+		port: env.server.port,
+		host: env.server.host,
+		base: './server',
+		router: './vendor/wp-cli/server-command/router.php'
+	}, () => {
+		backstopjs(command).then(() => {
+			return server.closeServer();
+		}).catch(() => {
+			server.closeServer();
+			return process.exit(1)
 		});
-	}
-	catch (error) {
-		return process.exit(1)
-	}
+	});
+
 };
 
 gulp.task('test', ['build', 'cloneTheme'] , () => {
