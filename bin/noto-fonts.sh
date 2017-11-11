2b4d298fbbf4d77368c3d92c138628d48cd26a91
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-11-11 17:57:53 +0900

    fix font install script

--- bin/noto-fonts.sh
+++ bin/noto-fonts.sh
@@ -5,7 +5,9 @@ set -e
 if [ ! -e $HOME/.noto ]; then
 	mkdir $HOME/.noto
 	curl https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip > $HOME/.noto/NotoSansCJKjp-hinted.zip
-	unzip $HOME/.noto/NotoSansCJKjp-hinted.zip "*.otf"
-	mkdir /usr/share/fonts/NotoSansCJKjp
-	mv NotoSans* /usr/share/fonts/NotoSansCJKjp/
 fi
+
+unzip $HOME/.noto/NotoSansCJKjp-hinted.zip "*.otf"
+mkdir /usr/share/fonts/opentype/noto
+mv NotoSans* /usr/share/fonts/opentype/noto/
+fc-cache -f -v
